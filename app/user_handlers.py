50077d37efd63901f7f5873b9f0252f2e27b5a73
Maveric-k07 <gurramakhileshwar333@gmail.com>
2022-11-09 20:21:59 +0530

    upload

---
+++ app/user_handlers.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, Depends
+from .auth import get_current_user
+
+router = APIRouter()
+
+
+@router.get("/test")
+async def test(username: str = Depends(get_current_user)):
+    return {"username": username}
